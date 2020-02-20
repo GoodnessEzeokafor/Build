830da6c7e174520766085cdf5d56d8d03cf07207
Goodness Ezeokafor <gootech442@yahoo.com>
2020-02-20 20:32:55 +0100

    new changes

---
+++ precache-manifest.e9f84994c621ff3cb4e775307d6692fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f15ba4a7c5208174e0722ff5e00543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60b90a93831ff468259c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff6d97a2cb9a6ff2cb34",
+    "url": "/static/js/2.610214de.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.610214de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60b90a93831ff468259c",
+    "url": "/static/js/main.919df0b0.chunk.js"
+  },
+  {
+    "revision": "cccdb754966eb5ea7e67",
+    "url": "/static/js/runtime-main.41b5d3ef.js"
+  }
+]);
