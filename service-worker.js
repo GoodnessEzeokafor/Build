901676ca792ab7dea94bb2dac387c06565e71518
Goodness Ezeokafor <gootech442@yahoo.com>
2020-05-01 09:32:20 +0100

    Deployment

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50c7a8ca4278059568a9378b561ce328.js"
+  "/precache-manifest.381dda15ad428631a6353783833e6e85.js"
 );
 
 self.addEventListener('message', (event) => {
